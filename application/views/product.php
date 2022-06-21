74ab7db38d685d4aee587f879dbc57100a254af7
babarali7 <babar_ali009@hotmail.com>
2022-06-21 13:18:35 +0500

    conversion of curreny euro to usd and ron

--- application/views/product.php
+++ application/views/product.php
@@ -37,6 +37,62 @@
 
 ?>
 
+<?php
+
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => "https://api.apilayer.com/exchangerates_data/convert?to=RON&from=EUR&amount=10",
+              CURLOPT_HTTPHEADER => array(
+                "Content-Type: text/plain",
+                "apikey: HFFyi8JW0bwZMEyngCVuiOEWd9MQ2Iri"
+              ),
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => "",
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => "GET"
+            ));
+
+            $response = curl_exec($curl);
+
+            $exchangeRates = json_decode($response, true);
+
+            // Access the exchange rate values, e.g. RON:
+            $ron_rate =  $exchangeRates['result'];
+
+
+
+            $curl_u = curl_init();
+
+            curl_setopt_array($curl_u, array(
+              CURLOPT_URL => "https://api.apilayer.com/exchangerates_data/convert?to=USD&from=EUR&amount=10",
+              CURLOPT_HTTPHEADER => array(
+                "Content-Type: text/plain",
+                "apikey: HFFyi8JW0bwZMEyngCVuiOEWd9MQ2Iri"
+              ),
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => "",
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => "GET"
+            ));
+
+            $response_u = curl_exec($curl_u);
+
+            $exchangeRates_u = json_decode($response_u, true);
+
+            // Access the exchange rate values, e.g. USD:
+            $usd_rate =  $exchangeRates_u['result'];
+
+         
+         ?>
+
+
 
 <div class="jumbotron text-center">
   <h1>Project Upwork Test</h1>
@@ -77,16 +133,25 @@
       <h4>Summarize price by user </h4>
       <?php
        
-       foreach($summarize_price_products_per_user as $sppu):  ?>     
-     
+       foreach($summarize_price_products_per_user as $sppu):  ?>          
         <p> <?=$sppu->user_fullname;?> - <?=$sppu->sumarize;?>$  </p>
-
-    
-     <?php  endforeach; ?>
-    
-      
+      <?php  endforeach; ?>     
     </div>
+  </div>
+  <div class="row">
+      <div class="col-sm-12" style="text-align:center">
+         <h2> Conversion of EURO to USD and RON </h2>
+          <div class="row">
+            <div class="col-sm-6">
+              <h4> 10 EUR = <?=$ron_rate;?> RON </h4>
+            </div>
 
+            <div class="col-sm-6">
+              <h4> 10 EUR = <?=$usd_rate;?> USD </h4>
+            </div>
+          </div>    
+
+      </div> 
   </div>
 </div>
 
